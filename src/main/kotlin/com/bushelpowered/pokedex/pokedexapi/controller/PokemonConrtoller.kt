3f6072fc846917ce4dfc043455df60c47bc96d9d
Joshua Molden <joshuamolden@Joshuas-MBP.fargo.myriad.local>
2021-06-22 12:24:37 -0500

    fixed bug with id endpoint

--- src/main/kotlin/com/bushelpowered/pokedex/pokedexapi/controller/PokemonConrtoller.kt
+++ src/main/kotlin/com/bushelpowered/pokedex/pokedexapi/controller/PokemonConrtoller.kt
@@ -20,10 +20,15 @@ import org.springframework.web.bind.annotation.*
 class PokemonConrtoller(val pokemonService: PokemonService,
                         val csvService: CsvService) {
 
+    // imports pokemon once
    @PostMapping("/import")
-   fun testImport(): ResponseEntity<Boolean> {
-       val result = csvService.importPokemon(ObjectMapper())
-       return ResponseEntity.ok().body(result)
+   fun testImport(): ResponseEntity<String> {
+       var bodyInfo = "Pokemon Imported"
+       when(pokemonService.getPokemonById(1)) {
+           null ->  csvService.importPokemon(ObjectMapper())
+           else -> bodyInfo = "Pokemon Already imported"
+       }
+       return ResponseEntity.ok().body(bodyInfo)
    }
 
     @GetMapping("/")
@@ -37,11 +42,11 @@ class PokemonConrtoller(val pokemonService: PokemonService,
     @GetMapping("/{pokemon_id}")
     fun getSpecificPokemonEntity (@PathVariable pokemon_id: Int): ResponseEntity<PokemonDetailsResponse>? {
         val returnPokemon = pokemonService.getPokemonById(pokemon_id)
-        val httpCode = if (returnPokemon!!.equals(null)) HttpStatus.OK else HttpStatus.NOT_FOUND
-//        val httpCode = when(returnPokemon) {
-//           null -> HttpStatus.NOT_FOUND
-//           else -> HttpStatus.OK
-//        }
+//        val httpCode = if (returnPokemon!!.equals(null)) HttpStatus.OK else HttpStatus.NOT_FOUND      can't figure out how to make if function like the when block of code
+        val httpCode = when(returnPokemon) {
+           null -> HttpStatus.NOT_FOUND
+           else -> HttpStatus.OK
+        }
         return ResponseEntity<PokemonDetailsResponse>(returnPokemon?.toDomain()?.toDetailsResponse(), httpCode)
     }
 }
